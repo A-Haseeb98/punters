e4095e49e4c5f9ecc0a532f4f7e1f4a594239d81
a-haseeb98 <haseeb.dar.dev@gmail.com>
2022-08-23 13:52:48 +0500

    xcd

--- src/components/cover/index.js
+++ src/components/cover/index.js
@@ -1,15 +1,13 @@
 import cover from '../../assets/images/cover.png'
 import './style.css'
-
+import MintBox from './mint_box'
 function Cover() {
     return (
         <div className='main_cover_container'>
             <div className='cover_container'>
                 <img src={cover} alt="cover" />
                 <div className='headline_container'>
-                    <h1>Not Just An NFT: A Complete Immersive Horse Racing Gaming & Betting Experience. Ready To Go When You Join</h1>
-                    <button>JOIN OUR DISCORD</button>
-
+                    <MintBox />
                 </div>
             </div>
             <div className='etherium_heading' id='about'>
---
+++ src/components/cover/mint_box/index.js
@@ -0,0 +1,63 @@
+import './index.css'
+
+import { HiPlus, HiMinus } from 'react-icons/hi'
+import { RiCloseFill } from 'react-icons/ri'
+import { useState } from 'react'
+import { MdDoubleArrow } from 'react-icons/md';
+
+function MintBox() {
+  let price = 0.09;
+  const [quantity, setQuantity] = useState(1)
+  const [close, setClose] = useState(false)
+  let clickHandler = (num, sign) => {
+    if (quantity < 3 && sign === '+') {
+      setQuantity(quantity + num)
+    }
+    else if (quantity > 1 && sign === '-') {
+      setQuantity(quantity - num)
+    }
+  }
+
+  return (
+    <div className='mint_container'>
+      <div className="message_box">
+        <p className='alert-p'>Minting Is Opened <br /> For Whitelisted Addresses. </p>
+      </div>
+      {
+        close ? <div className='notification_box'>
+          <p><span className='type'>Error! </span> Email address is already registered.</p>
+          <span onClick={() => setClose(false)} className='cut'><RiCloseFill color='white' /></span>
+        </div> : null
+      }
+
+      <div className="sellector_buttons_wrapper">
+        <button className='plus_minus_button' onClick={() => clickHandler(1, '-')}>
+          <HiMinus  />
+        </button>
+        <div >
+          <span className='quantity'> QUANTITY</span>
+          <span className='mint_qantity'>{quantity}</span>
+
+        </div>
+
+        <button className='plus_minus_button' onClick={() => clickHandler(1, '+')}>
+          <HiPlus  />
+        </button>
+      </div>
+      <p className='mint_price'>
+        {(price * quantity).toFixed(2)} ETH
+      </p>
+      <p className="text-center last_para text-white ">
+        You can mint up to 3 NFT per transaction. <br />
+      </p>
+      <button
+        onClick={() => setClose(true)}
+        className="connect_btn" >
+        <span><MdDoubleArrow /></span> MINT NOW
+      </button>
+    </div>
+
+  )
+}
+
+export default MintBox;
